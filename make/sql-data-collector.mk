788343f61d6724281874c4763647a303925159ba
chris-sun-star <hongxin.sun.vip@gmail.com>
2025-09-22 16:03:41 +0800

    add docker file and build command

--- make/sql-data-collector.mk
+++ make/sql-data-collector.mk
@@ -2,6 +2,7 @@
 
 COLLECTOR_BINARY_NAME ?= sql-data-collector
 COLLECTOR_BUILD_DIR ?= bin/
+COLLECTOR_IMG ?= oceanbase/ob-sql-data-collector
 
 .PHONY: build-sql-data-collector
 build-sql-data-collector: ## Build sql-data-collector
@@ -11,3 +12,8 @@ build-sql-data-collector: ## Build sql-data-collector
 .PHONY: run-sql-data-collector
 run-sql-data-collector: ## Run sql-data-collector in dev mode
 	@go run ./cmd/sql-data-collector
+
+.PHONY: docker-build-sql-data-collector
+docker-build-sql-data-collector: ## Build docker image for sql-data-collector.
+	@echo "Building docker image for $(COLLECTOR_BINARY_NAME)..."
+	@docker build -t $(COLLECTOR_IMG):$(VERSION) -f build/Dockerfile.sql-data-collector .
