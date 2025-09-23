4fc393ea2dea9c064dcd742b05a0eee185470500
chris-sun-star <hongxin.sun.vip@gmail.com>
2025-09-23 11:31:26 +0800

    add config for sql collector image

--- make/sql-data-collector.mk
+++ make/sql-data-collector.mk
@@ -1,3 +1,6 @@
+SQL_COLLECTOR_VERSION ?= 0.1.0
+SQL_COLLECTOR_IMG ?= quay.io/oceanbase/sql-data-collector:${SQL_COLLECTOR_VERSION}
+
 .PHONY: build-sql-data-collector
 build-sql-data-collector: ## Build sql-data-collector binary.
 	@echo Building sql-data-collector...
@@ -5,6 +8,7 @@ build-sql-data-collector: ## Build sql-data-collector binary.
 	@go build -o bin/sql-data-collector cmd/sql-data-collector/main.go
 
 .PHONY: docker-build-sql-data-collector
-docker-build-sql-data-collector: ## Build docker image for sql-data-collector.
-	@echo Building docker image for sql-data-collector...
-	@docker build -t sql-data-collector:latest -f build/Dockerfile.sql-data-collector . --build-arg GOPROXY=$(GOPROXY)
+docker-build-sql-data-collector: ## build sql-data-collector image
+	$(eval DOCKER_BUILD_ARGS :=)
+	$(if $(GOPROXY),$(eval DOCKER_BUILD_ARGS := --build-arg GOPROXY=$(GOPROXY)))
+	docker build $(DOCKER_BUILD_ARGS) -t ${SQL_COLLECTOR_IMG} -f build/Dockerfile.sql-data-collector .
