344e914d853a7a486e48414e503201a9ef483fc6
chris-sun-star <hongxin.sun.vip@gmail.com>
2025-09-23 11:06:53 +0800

    add goproxy

--- make/sql-data-collector.mk
+++ make/sql-data-collector.mk
@@ -1,19 +1,9 @@
-##@ sql-data-collector
-
-COLLECTOR_BINARY_NAME ?= sql-data-collector
-COLLECTOR_BUILD_DIR ?= bin/
-COLLECTOR_IMG ?= oceanbase/ob-sql-data-collector
-
 .PHONY: build-sql-data-collector
-build-sql-data-collector: ## Build sql-data-collector
-	@echo "Building $(COLLECTOR_BINARY_NAME)..."
-	@go build -o $(COLLECTOR_BUILD_DIR)$(COLLECTOR_BINARY_NAME) ./cmd/sql-data-collector
-
-.PHONY: run-sql-data-collector
-run-sql-data-collector: ## Run sql-data-collector in dev mode
-	@go run ./cmd/sql-data-collector
+build-sql-data-collector: ## Build sql-data-collector binary.
+	@echo Building sql-data-collector...
+	@go build -o $(BIN_DIR)/sql-data-collector $(ROOT_DIR)/cmd/sql-data-collector
 
 .PHONY: docker-build-sql-data-collector
 docker-build-sql-data-collector: ## Build docker image for sql-data-collector.
-	@echo "Building docker image for $(COLLECTOR_BINARY_NAME)..."
-	@docker build -t $(COLLECTOR_IMG):$(VERSION) -f build/Dockerfile.sql-data-collector .
+	@echo Building docker image for sql-data-collector...
+	@docker build -t sql-data-collector:latest -f build/Dockerfile.sql-data-collector . --build-arg GOPROXY=$(GOPROXY)
