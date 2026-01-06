ee790c444df2d590ec3de6359e0511e544d72fc1
chris-sun-star <hongxin.sun.vip@gmail.com>
2026-01-06 16:23:01 +0800

    use varchar to store plan hash

--- internal/sql-analyzer/const/sql/sql_plan.go
+++ internal/sql-analyzer/const/sql/sql_plan.go
@@ -24,7 +24,7 @@ const (
 		PLAN_ID            BIGINT,
 		SQL_ID             VARCHAR,
 		DB_ID              BIGINT,
-		PLAN_HASH          UBIGINT,
+		PLAN_HASH          VARCHAR,
 		GMT_CREATE         VARCHAR,
 		OPERATOR           VARCHAR,
 		OBJECT_NODE        VARCHAR,
