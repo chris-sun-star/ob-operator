f35d229fe6d598c23b43e0a0d3391a52eeb05508
chris-sun-star <hongxin.sun.vip@gmail.com>
2026-01-12 14:26:14 +0800

    delete proxy

--- ui/config/config.ts
+++ ui/config/config.ts
@@ -19,10 +19,4 @@ export default defineConfig({
   history: { type: 'hash' },
   npmClient: 'yarn',
   jsMinifier: 'terser',
-  proxy: {
-    '/api': {
-      target: 'http://11.161.204.50:30275',
-      changeOrigin: true,
-    },
-  },
 });
